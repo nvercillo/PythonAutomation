111faf353b43ab8ffd606be0b9280eef405fdf20
Stefan Vercillo <svercillo7@gmail.com>
2020-05-19 12:25:24 -0400

    Tue, May 19, 2020 12:25:24 PM

--- gitpush.sh
+++ gitpush.sh
@@ -6,7 +6,7 @@ RANGE=$(($CEILING-$FLOOR+1));
 STR=$(date)
 echo '.................
 
-Commit Time EST: 2323232/05/2020, 00:58:29
+Commit Time EST: ' $STR'
 
 ..................' > timesOfGitCommits.txt
 cat timesOfGitCommits.txt >> temp_file.txt
