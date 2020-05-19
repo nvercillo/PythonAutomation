ee8cff8b2f79f5336ba2d4e76089d837abef8cdf
Stefan Vercillo <svercillo7@gmail.com>
2020-05-19 16:28:29 -0400

    Tue May 19 16:28:29 EDT 2020

--- randnum.pl
+++ randnum.pl
@@ -1,3 +1,4 @@
-open (OVERWRITE, ">rand.txt") or die "$! You deleted random number file! Make a new one (rand.txt)!!!!!!!";;
-$circle=int(rand(23) +9);
-print OVERWRITE $circle;
+#!/usr/bin/perl
+# open (OVERWRITE, ">rand.txt") or die "$! You deleted random number file! Make a new one (rand.txt)!!!!!!!";;
+# $circle=int(rand(23) +9);
+# print OVERWRITE $circle;
---
+++ runPushes.bat
@@ -0,0 +1,1 @@
+start "gitbash" "%PROGRAMFILES%\Git\bin\sh.exe"  -i -c "bash gitpush.sh;  exec bash"
