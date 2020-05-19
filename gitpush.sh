d55d83437f13900a69c028c41db76754a5dabb47
Stefan Vercillo <svercillo7@gmail.com>
2020-05-18 22:54:37 -0400

    Automated commit

--- gitpush.sh
+++ gitpush.sh
@@ -1,9 +1,9 @@
-git add .
-git commit -m "Automated commit"
 echo '.................
 
 Commit Time EST: 05/05/2020, 00:58:29
 
 ..................' > temp_file.txt
 cat timesOfGitCommits.txt >> temp_file.txt
+git add .
+git commit -m "Automated commit"
 git push
