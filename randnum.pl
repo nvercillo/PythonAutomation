a7b83fa5a058e094f7a2850ad8a343f9eec22af2
Stefan Vercillo <svercillo7@gmail.com>
2020-05-19 19:52:20 -0400

    Tue May 19 19:52:20 EDT 2020

--- randnum.pl
+++ randnum.pl
@@ -1,4 +1,4 @@
 #!/usr/bin/perl
-# open (OVERWRITE, ">rand.txt") or die "$! You deleted random number file! Make a new one (rand.txt)!!!!!!!";;
-# $circle=int(rand(23) +9);
-# print OVERWRITE $circle;
+open (OVERWRITE, ">rand.txt") or die "$! You deleted random number file! Make a new one (rand.txt)!!!!!!!";;
+$circle=int(rand(23) +9);
+print OVERWRITE $circle;
