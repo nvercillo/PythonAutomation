d45876e04bee4b14aebc3c5fdcb7b659f509e32d
Stefan Vercillo <svercillo7@gmail.com>
2020-05-19 12:24:26 -0400

    Tue, May 19, 2020 12:24:26 PM

--- gitpush.sh
+++ gitpush.sh
@@ -3,7 +3,7 @@ CEILING=100;
 RANGE=$(($CEILING-$FLOOR+1));
 
 
-stirng = date
+STR=$(date)
 echo '.................
 
 Commit Time EST: 2323232/05/2020, 00:58:29
@@ -11,5 +11,5 @@ Commit Time EST: 2323232/05/2020, 00:58:29
 ..................' > timesOfGitCommits.txt
 cat timesOfGitCommits.txt >> temp_file.txt
 git add .
-git commit -m string
+git commit -m "$STR"
 git push
