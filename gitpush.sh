ed0f335b698102ac19b10392916f0d479f260e19
Stefan Vercillo <svercillo7@gmail.com>
2020-05-18 22:33:06 -0400

    Automated commit

--- gitpush.sh
+++ gitpush.sh
@@ -1,13 +1,13 @@
 git add .
 
 echo 'Enter the commit message:'
-read commitMessage
+# read commitMessage
 
-git commit -m "$commitMessage"
+git commit -m "Automated commit"
 
 echo 'Your git pushing the current branch, make sure its not master'
 # read branch
 # git push origin $branch
 git push
 
-read
+# read
