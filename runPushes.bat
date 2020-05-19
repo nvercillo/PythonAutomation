7eca70a1270e342a2cc54ba74333046cf790c041
Stefan Vercillo <svercillo7@gmail.com>
2020-05-19 16:54:09 -0400

    Tue May 19 16:54:09 EDT 2020

--- runPushes.bat
+++ runPushes.bat
@@ -1,1 +1,1 @@
-start "gitbash" "%PROGRAMFILES%\Git\bin\sh.exe"  -i -c "bash gitpush.sh;  exec bash"
+start "gitbash" "%PROGRAMFILES%\Git\bin\sh.exe"  -i -c "home; cd PythonAutomation; bash gitpush.sh;  exec bash"
