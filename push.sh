c864e4814965ad7088088ace4e612291899433be
Stefan Vercillo <svercillo7@gmail.com>
2021-01-23 23:27:18 -0500

    Automated Commit at 1611462438

--- push.sh
+++ push.sh
@@ -5,12 +5,18 @@ d=`date +%s`
 log_file="./logs/log_$d.txt"
 
 if [[ -n "$name" ]]; then
+
     for i in $(seq "$1")
         do
-            echo "This is an automated message" > $log_file
-            git -A 
-            git commit -m "Automated Commit at $d" 
-            git push
+            if [$1 -gt 0]
+            then 
+                echo "SDfsdfsdf"
+                rm -rf logs
+            fi
+            # echo "This is an automated message" > $log_file
+            # git add $log_file
+            # git commit -m "Automated Commit at $d" 
+            # git push
         done
 else
     echo "argument error"
