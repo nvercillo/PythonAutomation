baed68ffb0f2eb02eb7a8e6e8dc3d7cfc47aa144
Stefan Vercillo <svercillo7@gmail.com>
2021-01-24 01:56:57 -0500

    SDSDFSDF

--- gitpush.py
+++ gitpush.py
@@ -1,12 +1,10 @@
+#!/usr/bin/python
 import subprocess
 import os
 import random
 
 
 num = random.randint(1, 6)
-f = open("demofile2.txt", "a")
-f.write("Now the file has more content!")
-f.close()
 print(num)
 subprocess.check_call(["./push.sh", str(num)])
 
